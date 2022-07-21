19778dc53be0e08c722c7c8e7471609effe4f684
김영은 <99572560+youngeun10@users.noreply.github.com>
2022-07-21 19:00:38 +0900

    [Silver IV] Title: 제로, Time: 120 ms, Memory: 31620 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,8 +1,9 @@
-n = int(input())
+from sys import stdin
+
 arr = []
 
-for _ in range(n):
-    n = int(input())
+for _ in range(int(stdin.readline().strip())):
+    n = int(stdin.readline().strip())
     arr.pop() if n == 0 else arr.append(n)
 
 print(sum(arr))
