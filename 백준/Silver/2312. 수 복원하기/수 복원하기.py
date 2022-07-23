249f2a09065095e3a47e5cb6e076b3bcb5eec66b
김영은 <99572560+youngeun10@users.noreply.github.com>
2022-07-23 19:18:20 +0900

    [Silver III] Title: 수 복원하기, Time: 240 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2312. 수 복원하기/수 복원하기.py
+++ 백준/Silver/2312. 수 복원하기/수 복원하기.py
@@ -1,19 +1,22 @@
+def reconstruction(num):
+    x = 2
+    dic = {}
+    while num != 1:
+        if num % x == 0:
+            if x in dic:
+                dic[x] += 1
+            else:
+                dic[x] = 1
+            num //= x
+        else:
+            x += 1
+    return dic
+
 import sys
 
 n = int(sys.stdin.readline().strip())
 
 for _ in range(n):
-    a = int(sys.stdin.readline().strip())
-    x = 2
-    cnt = []
-    while a != 1:
-        if a % x == 0:
-            a = a // x
-            cnt.append(x)
-        else:
-            x += 1
-
-    tmp = list(set(cnt))
-    tmp.sort()
-    for i in tmp:
-        print(i, cnt.count(i))
+    d = reconstruction(int(sys.stdin.readline().strip()))
+    for k, v in d.items():
+        print(k, v)
