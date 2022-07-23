b4c97e85b6cbff256c824c063d75b2d435970f55
김영은 <99572560+youngeun10@users.noreply.github.com>
2022-07-23 18:40:35 +0900

    [Silver IV] Title: 카드셋트, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11507. 카드셋트/카드셋트.py
+++ 백준/Silver/11507. 카드셋트/카드셋트.py
@@ -1,35 +1,16 @@
 import sys
 
 card = sys.stdin.readline().strip()
-cnt = [
-        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-        , [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-        , [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-        , [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-    ]
-result = []
+cnt = {'P': [], 'K': [], 'H': [], 'T': []}
 
 for i in range(len(card)//3):
-
-    if card[(i*3)] == 'P':
-        sign = 0
-        n = int(card[(i * 3) + 1:(i * 3) + 3])
-    elif card[(i*3)] == 'K':
-        sign = 1
-        n = int(card[(i * 3) + 1:(i * 3) + 3])
-    elif card[(i*3)] == 'H':
-        sign = 2
-        n = int(card[(i * 3) + 1:(i * 3) + 3])
-    elif card[(i*3)] == 'T':
-        sign = 3
-        n = int(card[(i * 3) + 1:(i * 3) + 3])
-
+    n = card[(i*3):(i*3)+3]
+    sign = card[i*3]
+    
     if n in cnt[sign]:
-        cnt[sign].remove(n)
-    else:
         print("GRESKA")
         quit()
+    else:
+        cnt[sign].append(n)
 
-for i in range(4):
-    result.append(len(cnt[i]))
-print(*result)
+print(13-len(cnt['P']), 13-len(cnt['K']), 13-len(cnt['H']), 13-len(cnt['T']))
