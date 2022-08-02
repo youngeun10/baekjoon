3e52457981c0ead0f98fbbc0a4a42086d310f229
김영은 <99572560+youngeun10@users.noreply.github.com>
2022-08-02 16:05:02 +0900

    [Silver V] Title: 폴리오미노, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -1,18 +1,7 @@
-def polyomino(s, a, b):
-    while True:
-        if s.find(a) >= 0:
-            s = s.replace(a, b)
-        else:
-            break
-    return s
-
 from sys import stdin
 board = stdin.readline().rstrip()
 
-board = polyomino(board, 'XXXX', 'AAAA')
-board = polyomino(board, 'XX', 'BB')
+board = board.replace('XXXX', 'AAAA')
+board = board.replace('XX', 'BB')
 
-if board.find('X') >= 0:
-    print(-1)
-else:
-    print(board)
+print(-1) if 'X' in board else print(board)
