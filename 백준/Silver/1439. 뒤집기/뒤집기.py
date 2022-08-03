2cb74e20e14491b89d2c45cde928f2cbe03ceb2d
김영은 <99572560+youngeun10@users.noreply.github.com>
2022-08-03 09:43:50 +0900

    [Silver V] Title: 뒤집기, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,17 +1,6 @@
-def change(arr, c, d):
-    z = len(arr) - 1
-    cnt = 0
-    while c in s:
-        if c * z in s:
-            cnt += arr.count(c * z)
-            arr = arr.replace(c * z, d * z)
-            z -= 1
-        else:
-            z -= 1
-
-        if z < 1:
-            break
-    return cnt
-
-s = input()
-print(min(change(s, '0', '1'), change(s, '1', '0')))
+cnt = 1
+string = input()
+for i in range(1, len(string)):
+    if string[i] != string[i-1]:
+        cnt += 1
+print(cnt//2)
