142803ca7678b84a6f10bbdf15c964488c951ccd
김영은 <99572560+youngeun10@users.noreply.github.com>
2022-09-04 21:43:26 +0900

    [Silver V] Title: 5학년은 다니기 싫어요, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/23028. 5학년은 다니기 싫어요/5학년은 다니기 싫어요.py
+++ 백준/Silver/23028. 5학년은 다니기 싫어요/5학년은 다니기 싫어요.py
@@ -1,10 +1,8 @@
 from sys import stdin
-from collections import deque
 
 n, a, b = map(int, stdin.readline().rstrip().split())
-sub = deque(list(map(int, stdin.readline().rstrip().split())) for _ in range(10))
-for _ in range(8-n):
-    i, j = sub.popleft()
+sub = [list(map(int, stdin.readline().rstrip().split())) for _ in range(10)]
+for i, j in sub[:8-n]:
     a += i*3
-    b += 6*3 if i+j > 6 else (i+j)*3 
+    b += 6*3 if i+j > 6 else (i+j)*3
 print("Nice") if a >= 66 and b >= 130 else print("Nae ga wae")
