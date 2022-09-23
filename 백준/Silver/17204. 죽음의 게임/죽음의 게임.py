8d4918c8590f9edb2649a40a888bf7956a98919e
김영은 <99572560+youngeun10@users.noreply.github.com>
2022-09-23 16:44:37 +0900

    [Silver III] Title: 죽음의 게임, Time: 88 ms, Memory: 32432 KB -BaekjoonHub

---
+++ 백준/Silver/17204. 죽음의 게임/죽음의 게임.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+
+def bfs(v):
+    queue = deque([v])
+    cnt = 0
+    for _ in range(n):
+        target = queue.popleft()
+        cnt += 1
+        
+        if graph[target] == k:
+            return cnt
+        
+        queue.append(graph[target])
+    return -1
+
+
+n, k = map(int, sys.stdin.readline().split())
+graph = [int(sys.stdin.readline()) for _ in range(n)]
+
+print(bfs(0))
