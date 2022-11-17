0e0f0558adeda63074a0e6f458c002802b3450ce
김영은 <99572560+youngeun10@users.noreply.github.com>
2022-11-17 15:38:45 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+n = int(stdin.readline())
+dp = [[1 for _ in range(10)] for _ in range(n)]
+dp[0][0] = 0
+
+for i in range(1, n):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][j+1]
+        elif j == 9:
+            dp[i][j] = dp[i-1][j-1]
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+
+print(sum(dp[n-1]) % 1000000000)
