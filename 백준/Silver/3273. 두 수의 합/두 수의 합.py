5e044396bb23b292af4282f1d0c062be32c19624
김영은 <99572560+youngeun10@users.noreply.github.com>
2022-12-01 21:03:13 +0900

    [Silver III] Title: 두 수의 합, Time: 104 ms, Memory: 41620 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -3,28 +3,12 @@ from sys import stdin
 n = int(stdin.readline())
 arr = list(map(int, stdin.readline().split()))
 x = int(stdin.readline())
-
-arr.sort()
-
-if n <= 2:
-    print(1) if n == 2 and sum(arr) == x else print(0)
-    exit()
-
-i, j = 0, n-1
+h = set()
 cnt = 0
 
-while True:
-    if i >= j:
-        break
-
-    if arr[i] + arr[j] == x:
-        i += 1
-        j -= 1
+for i in arr:
+    if i in h:
         cnt += 1
-    elif arr[i] + arr[j] > x:
-        j -= 1
-    else:
-        i += 1
-        j = n-1
-
+    elif i <= x:
+        h.add(x-i)
 print(cnt)
