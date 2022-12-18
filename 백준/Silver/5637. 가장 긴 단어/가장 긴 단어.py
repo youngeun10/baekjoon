7a312f7245abab3259a1857049fd580a23f027b2
김영은 <99572560+youngeun10@users.noreply.github.com>
2022-12-18 14:06:57 +0900

    [Silver IV] Title: 가장 긴 단어, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/5637. 가장 긴 단어/가장 긴 단어.py
+++ 백준/Silver/5637. 가장 긴 단어/가장 긴 단어.py
@@ -1,5 +1,23 @@
-import re
+from sys import stdin
 
-arr = re.findall('[A-Z\-a-z]+', open(0).read()[:-1])
-arr.sort(key=lambda x: len(x), reverse=True)
-print(arr[0].lower())
+result = ''
+chk = 0
+f = "1234567890.,()\""
+
+while chk == 0:
+    s = stdin.readline().rstrip()
+    for c in f:
+        s = s.replace(c, "")
+
+    tmp = list(s.split())
+    if 'E-N-D' in tmp:
+        chk = 1
+        tmp.pop()
+
+    tmp.sort(key=lambda x: len(x), reverse=True)
+
+    if len(tmp) > 0:
+        if len(tmp[0]) > len(result):
+            result = tmp[0]
+
+print(result.lower())
