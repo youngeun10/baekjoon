e683410c69208150733eb59d0fbf158f442e2f31
김영은 <99572560+youngeun10@users.noreply.github.com>
2023-01-29 16:47:40 +0900

    [Silver IV] Title: 설탕 배달, Time: 260 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,12 +1,9 @@
 from sys import stdin
 
-# 3키로, 5키로 봉지가 있음.
 n = int(stdin.readline())
 r = (n//5) + 1
 c = (n//3) + 1
 result = 10**9
-
-dp = [[0] * r for _ in range(c)]
 
 for i in range(r):
     for j in range(c):
