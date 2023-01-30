8baab3af723173e3b8642d554742eecfb64c6ad5
김영은 <99572560+youngeun10@users.noreply.github.com>
2023-01-30 13:16:35 +0900

    [Silver III] Title: 통계학, Time: 224 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,34 +1,28 @@
 from sys import stdin
-
-n = int(stdin.readline())
-arr = [0] * n
-d = {}      # 숫자별 count
-dcnt = {}
-sum = 0
-
-for i in range(n):
-    arr[i] = int(stdin.readline())
-    sum += arr[i]
-
-    if arr[i] not in d:
-        d[arr[i]] = 1
-        if 1 not in dcnt:
-            dcnt[1] = [arr[i]]
-        else:
-            dcnt[1].append(arr[i])
-    else:
-        d[arr[i]] += 1
-        if d[arr[i]] not in dcnt:
-            dcnt[d[arr[i]]] = [arr[i]]
-        else:
-            dcnt[d[arr[i]]].append(arr[i])
-
-arr.sort()
-tmp = dcnt[max(d.values())]
-tmp.sort()
-
-
-print(round(sum/n))
-print(arr[n//2])
-print(tmp[1]) if len(tmp) > 1 else print(tmp[0])
-print(arr[-1]-arr[0])
+input = stdin.readline
+cnt = int(input())
+nums = [0] * 8001
+for i in range(cnt):
+    nums[int(input())+4000] += 1
+avg = 0
+num_count = 0; center = 4001
+min_num = 4001; max_num = -4001
+for i in range(8001):
+    if nums[i] != 0:
+        avg += (i-4000) * nums[i]
+        num_count += nums[i]
+        if center == 4001 and num_count >= cnt/2:
+            center = i - 4000
+        if min_num > i - 4000:
+            min_num = i - 4000
+        if max_num < i - 4000:
+            max_num = i - 4000
+max_count = max(nums);
+max_count_num = nums.index(max_count) - 4000
+nums[max_count_num + 4000] = 0
+if max(nums) == max_count:
+    max_count_num = nums.index(max(nums)) - 4000
+print(round(avg/cnt))
+print(center)
+print(max_count_num)
+print(max_num - min_num)
