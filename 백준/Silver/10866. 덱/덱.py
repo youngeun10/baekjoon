c9e42367cfee4d78259b115206e811b146e8b92c
김영은 <99572560+youngeun10@users.noreply.github.com>
2023-02-01 11:27:04 +0900

    [Silver IV] Title: 덱, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,19 +1,18 @@
 from sys import stdin
-from collections import deque
 
-q = deque([])
+q = []
 
 for _ in range(int(stdin.readline())):
     t = stdin.readline().strip()
 
     if "push_front" in t:
-        q.appendleft(t[11:])
+        q.insert(0, t[11:])
     elif "push_back" in t:
         q.append(t[10:])
     elif "pop_front" in t:
-        print(q.popleft()) if len(q) > 0 else print(-1)
+        print(q.pop(0)) if len(q) > 0 else print(-1)
     elif "pop_back" in t:
-        print(q.pop()) if len(q) > 0 else print(-1)
+        print(q.pop(-1)) if len(q) > 0 else print(-1)
     elif "size" in t:
         print(len(q))
     elif "empty" in t:
@@ -21,4 +20,4 @@ for _ in range(int(stdin.readline())):
     elif "front" in t:
         print(q[0]) if len(q) > 0 else print(-1)
     elif "back" in t:
-        print(q[len(q)-1]) if len(q) > 0 else print(-1)
+        print(q[-1]) if len(q) > 0 else print(-1)
