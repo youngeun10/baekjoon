cbc82b819c7f0636cae6885ecf6f51d57be2029a
김영은 <99572560+youngeun10@users.noreply.github.com>
2023-02-01 11:23:55 +0900

    [Silver IV] Title: 덱, Time: 72 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,40 +1,24 @@
+from sys import stdin
 from collections import deque
-import sys
 
-input = sys.stdin.readline
+q = deque([])
 
-d = deque(list())
-result = []
-rear = -1
+for _ in range(int(stdin.readline())):
+    t = stdin.readline().strip()
 
-for _ in range(int(input())):
-    tmp = list(map(str, input().split()))
-
-    if tmp[0] == 'push_back':
-        d.append(tmp[1])
-        rear += 1
-    elif tmp[0] == 'push_front':
-        d.appendleft(tmp[1])
-        rear += 1
-    elif tmp[0] == 'pop_front':
-        if rear >= 0:
-            result.append(d.popleft())
-            rear -= 1
-        else:
-            result.append(str(-1))
-    elif tmp[0] == 'pop_back':
-        if rear >= 0:
-            result.append(d.pop())
-            rear -= 1
-        else:
-            result.append(str(-1))
-    elif tmp[0] == 'size':
-        result.append(rear+1)
-    elif tmp[0] == 'empty':
-        result.append(1 if len(d) == 0 else str(0))
-    elif tmp[0] == 'front':
-        result.append(d[0] if rear >= 0 else str(-1))
-    elif tmp[0] == 'back':
-        result.append(d[rear] if rear >= 0 else str(-1))
-
-print(*result, sep='\n')
+    if "push_front" in t:
+        q.appendleft(t[11:])
+    elif "push_back" in t:
+        q.append(t[10:])
+    elif "pop_front" in t:
+        print(q.popleft()) if len(q) > 0 else print(-1)
+    elif "pop_back" in t:
+        print(q.pop()) if len(q) > 0 else print(-1)
+    elif "size" in t:
+        print(len(q))
+    elif "empty" in t:
+        print(0) if len(q) > 0 else print(1)
+    elif "front" in t:
+        print(q[0]) if len(q) > 0 else print(-1)
+    elif "back" in t:
+        print(q[len(q)-1]) if len(q) > 0 else print(-1)
