89406e01d06357911132adb32a7abb8b1fde139d
김영은 <99572560+youngeun10@users.noreply.github.com>
2023-02-01 13:18:20 +0900

    [Silver II] Title: 에디터, Time: 244 ms, Memory: 37576 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -10,13 +10,13 @@ for _ in range(int(input())):
     c = input().strip()
 
     if 'L' in c:
-        if len(st1) > 0:
+        if st1:
             st2.append(st1.pop())
     elif 'D' in c:
-        if len(st2) > 0:
+        if st2:
             st1.append(st2.pop())
     elif 'B' in c:
-        if len(st1) > 0:
+        if st1:
             st1.pop()
     else:
         st1.append(c[2:])
