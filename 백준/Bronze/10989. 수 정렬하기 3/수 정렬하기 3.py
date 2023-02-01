9ab1c73c1ae48e1fe591c1468401e52920e50086
김영은 <99572560+youngeun10@users.noreply.github.com>
2023-02-01 10:50:15 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 6836 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,9 +1,13 @@
-from sys import stdin
+import sys
+
+input = sys.stdin.readline
+print = sys.stdout.write
+
 num_arr = [0] * 10001
-for _ in range(int(stdin.readline())):
-    num_arr[int(stdin.readline())] += 1
+for _ in range(int(input())):
+    num_arr[int(input())] += 1
 
 for i in range(1, 10001):
     if num_arr[i] > 0:
         for _ in range(num_arr[i]):
-            print(i)
+            print(str(i) + '\n')
