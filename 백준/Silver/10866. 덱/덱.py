75f2b605a96059e2df27febfa4c370700f3fcf66
김영은 <99572560+youngeun10@users.noreply.github.com>
2023-02-01 11:50:40 +0900

    [Silver IV] Title: 덱, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,23 +1,26 @@
-from sys import stdin
+import sys
+
+input = sys.stdin.readline
+print = sys.stdout.write
 
 q = []
 
-for _ in range(int(stdin.readline())):
-    t = stdin.readline().strip()
+for _ in range(int(input())):
+    t = input().strip()
 
     if "push_front" in t:
         q.insert(0, t[11:])
     elif "push_back" in t:
         q.append(t[10:])
     elif "pop_front" in t:
-        print(q.pop(0)) if len(q) > 0 else print(-1)
+        print(q.pop(0) + '\n') if len(q) > 0 else print('-1' + '\n')
     elif "pop_back" in t:
-        print(q.pop(-1)) if len(q) > 0 else print(-1)
+        print(q.pop(-1) + '\n') if len(q) > 0 else print('-1' + '\n')
     elif "size" in t:
-        print(len(q))
+        print(str(len(q)) + '\n')
     elif "empty" in t:
-        print(0) if len(q) > 0 else print(1)
+        print(str(0) + '\n') if len(q) > 0 else print('1' + '\n')
     elif "front" in t:
-        print(q[0]) if len(q) > 0 else print(-1)
+        print(q[0] + '\n') if len(q) > 0 else print('-1' + '\n')
     elif "back" in t:
-        print(q[-1]) if len(q) > 0 else print(-1)
+        print(q[-1] + '\n') if len(q) > 0 else print('-1' + '\n')
