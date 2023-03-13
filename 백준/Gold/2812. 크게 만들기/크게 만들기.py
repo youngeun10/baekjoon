f88379ecce04d8f6ccba32ea49266e76b2d0c89a
김영은 <99572560+youngeun10@users.noreply.github.com>
2023-03-13 12:51:46 +0900

    [Gold III] Title: 크게 만들기, Time: 172 ms, Memory: 38972 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.py
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -7,7 +7,7 @@ answer = []
 cnt = k
 for num in number:
     while answer and cnt > 0 and answer[-1] < num:
-        del answer[-1]
+        answer.pop()
         cnt -= 1
     answer.append(num)
 print(''.join(answer[:n-k]))
