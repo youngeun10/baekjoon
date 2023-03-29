defa48daa1ff03b03fe8c9271f8d0d36b04ec24c
김영은 <99572560+youngeun10@users.noreply.github.com>
2023-03-29 17:32:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.12 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,11 +1,6 @@
-def solution(participant, completion):
-    temp = 0
-    dic = {}
-    for part in participant:
-        dic[hash(part)] = part
-        temp += int(hash(part))
-    for com in completion:
-        temp -= hash(com)
-    answer = dic[temp]
+import collections
 
-    return answer
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
