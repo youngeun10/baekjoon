675521e454a3401caec7570d721fbf58dee5bab3
김영은 <99572560+youngeun10@users.noreply.github.com>
2023-08-14 14:57:11 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31364 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,31 +1,25 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 graph = [list(map(int, input().rstrip())) for _ in range(N)]
 dir = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-visited = [[False] * N for _ in range(N)]
-result = []
+
+def dfs(x, y, cnt):
+    graph[x][y] = 0
+    for dx, dy in dir:
+        _x, _y = x + dx, y + dy
+        if (0 <= _x < N and 0 <= _y < N) and graph[_x][_y]:
+            cnt = dfs(_x, _y, cnt+1)
+    return cnt
+
+cnt = []
 
 for r in range(N):
     for c in range(N):
-        cnt = 0
-        if visited[r][c] or graph[r][c] == 0:
-            continue
-        cnt += 1
-        visited[r][c] = True
-        queue = deque([(r, c)])
-        while queue:
-            _r, _c = queue.popleft()
-            for dr, dc in dir:
-                x, y = _r+dr, _c+dc
-                if not (0 <= x < N and 0 <= y <N) or visited[x][y] or not graph[x][y]:
-                    continue
-                cnt += 1
-                queue.append((x, y))
-                visited[x][y] = True
-        result.append(cnt)
+        if graph[r][c]:
+            cnt.append(dfs(r, c, 1))
 
-print(len(result))
-print(*sorted(result), sep="\n")
+cnt.sort()
+print(len(cnt))
+print(*cnt, sep="\n")
